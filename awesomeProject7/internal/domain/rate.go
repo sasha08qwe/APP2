faca2904d4814c5d8172f9089bdb1a2f683b9d98
sasha08qwe <postogrenka@yandex.ru>
2025-12-15 16:43:52 +0300

    Delete awesomeProject7 directory

--- awesomeProject7/internal/domain/rate.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-import "time"
-
-type Rate struct {
-	Currency  string    `json:"currency"`
-	Price     float64   `json:"price"`
-	Timestamp time.Time `json:"timestamp"`
-}
--- awesomeProject7/internal/ports/rate_provider.go
+++
@@ -1,7 +0,0 @@
-package ports
-
-import "project/internal/domain"
-
-type RateProvider interface {
-	FetchRate(currency string) (*domain.Rate, error)
-}
